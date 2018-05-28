470d3fe509ca809c971d692977ce68c52ea59d70
Todd Meinershagen <todd@meinershagen.net>
2018-05-28 02:49:13 -0500

    Updating the deployment script.

--- DeployToNuget.bat
+++ DeployToNuget.bat
@@ -15,17 +15,21 @@ mkdir build
 REM Requests the API Key
 REM ====================
 SET /p NuGetApiKey= Please enter the project's NuGet API Key: 
+nuget.exe setApiKey %NuGetApiKey%
 
 SET package="src\Bumblebee.KendoUI\Bumblebee.KendoUI.csproj"
 
 REM Create the Package
 REM ==================
+ECHO "Restoring project found here:  %package%."
+nuget.exe restore %package%
+
 ECHO "Packing/Pushing project found here:  %package%."
-nuget.exe pack -Build -OutputDirectory build %package% -Prop Configuration=Release
+dotnet build %package% -c Release
+dotnet pack %package% --no-build -o ..\..\build -c Release
 
 REM Push to Nuget 
 REM =============
 cd build
-
-nuget.exe push Bumblebee.Automation.KendoUI.*.nupkg %NuGetApiKey% -Source https://www.nuget.org/api/v2/package
+dotnet nuget push *.nupkg -s https://api.nuget.org/v3/index.json
 cd ..
